d165e7bb8c0e6f5eb82824775b5c2d392779aa8c
kherkman <kristian.herkman@gmail.com>
2025-09-16 22:46:26 +0300

    Update demos.js

--- demos.js
+++ demos.js
@@ -20,9 +20,9 @@ const demoConfigs = [
     {
         tempo: 100, skin: 'blue-wave', globalLoopBeats: 4, masterVolume: 0.75,
         sequencers: [
-            { id: 1, circles: [{ x: 0, velocity: 110, pan: 0 }, { x: 500, velocity: 110, pan: 0 }], channel: "1", note: "36", wavFileName: "Impact_1.wav", isMuted: false, volume: 0.8, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" },
+            { id: 1, circles: [{ x: 0, velocity: 110, pan: 0 }], channel: "1", note: "36", wavFileName: "Impact_1.wav", isMuted: false, volume: 0.4, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" },
             { id: 2, circles: [{ x: 350, velocity: 95, pan: -0.3 }, { x: 750, velocity: 95, pan: 0.3 }], channel: "2", note: "38", wavFileName: "Conga_Low.wav", isMuted: false, volume: 0.7, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" },
-            { id: 3, circles: [{ x: 200, velocity: 80, pan: -0.5 }, { x: 400, velocity: 80, pan: 0 }, { x: 600, velocity: 80, pan: 0.5 }, { x: 800, velocity: 80, pan: 0.7 }], channel: "3", note: "42", wavFileName: "Shaker.wav", isMuted: false, volume: 0.5, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" }
+            { id: 3, circles: [{ x: 200, velocity: 80, pan: -0.5 }, { x: 400, velocity: 80, pan: 0 }, { x: 600, velocity: 80, pan: 0.5 }, { x: 800, velocity: 80, pan: 0.7 }], channel: "3", note: "42", wavFileName: "Shaker.wav", isMuted: false, volume: 1.0, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" }
         ]
     }
 ];
