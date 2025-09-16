36ab8698b0a2557b50e553185e4162342d083a92
kherkman <kristian.herkman@gmail.com>
2025-09-16 23:27:58 +0300

    Update demos.js

--- demos.js
+++ demos.js
@@ -12,15 +12,16 @@ const demoConfigs = [
         isHumanizerOn: false, humanizeTiming: 0, humanizeVelocity: 0, humanizePan: 0,
         humanizerControlsCollapsed: false, effectsControlsCollapsed: false,
         sequencers: [
-            { id: 1, circles: [{ x: 0, velocity: 100, pan: 0 }, { x: 300, velocity: 100, pan: 0 }, { x: 600, velocity: 100, pan: 0 } ], channel: "1", note: "36", wavFileName: "Kick_01.wav", isMuted: false, volume: 0.75, pan: 0, phase: 0, editMode: "add"},
-            { id: 2, circles: [{ x: 200, velocity: 90, pan: -0.2 }, { x: 600, velocity: 90, pan: 0.2 }], division: "1/4", channel: "2", note: "38", wavFileName: "Shaker.wav", isMuted: false, volume: 0.6, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" }
+            { id: 1, circles: [{ x: 1, velocity: 100, pan: 0 }, { x: 500, velocity: 100, pan: 0 } ], channel: "1", note: "36", wavFileName: "Kick_01.wav", isMuted: false, volume: 0.5, pan: 0, phase: 0, editMode: "add"},
+            { id: 2, circles: [{ x: 1, velocity: 250, pan: 0 }, { x: 750, velocity: 100, pan: 0 } ], channel: "1", note: "36", wavFileName: "Snare_01.wav", isMuted: false, volume: 0.5, pan: 0, phase: 0, editMode: "add"},
+            { id: 3, circles: [{ x: 200, velocity: 90, pan: -0.2 }, { x: 600, velocity: 90, pan: 0.2 }], division: "1/4", channel: "2", note: "38", wavFileName: "Shaker.wav", isMuted: false, volume: 0.6, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" }
         ]
     },
     // Demo 2 
     {
         tempo: 100, skin: 'blue-wave', globalLoopBeats: 4, masterVolume: 0.75,
         sequencers: [
-            { id: 1, circles: [{ x: 0, velocity: 110, pan: 0 }], channel: "1", note: "36", wavFileName: "Impact_1.wav", isMuted: false, volume: 0.4, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" },
+            { id: 1, circles: [{ x: 1, velocity: 110, pan: 0 }], channel: "1", note: "36", wavFileName: "Impact_1.wav", isMuted: false, volume: 0.4, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" },
             { id: 2, circles: [{ x: 350, velocity: 95, pan: -0.3 }, { x: 750, velocity: 95, pan: 0.3 }], channel: "2", note: "38", wavFileName: "Conga_Low.wav", isMuted: false, volume: 0.7, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" },
             { id: 3, circles: [{ x: 200, velocity: 80, pan: -0.5 }, { x: 400, velocity: 80, pan: 0 }, { x: 600, velocity: 80, pan: 0.5 }, { x: 800, velocity: 80, pan: 0.7 }], channel: "3", note: "42", wavFileName: "Shaker.wav", isMuted: false, volume: 1.0, pan: 0, phase: 0, editMode: "add", presetRhythm: "off" }
         ]
